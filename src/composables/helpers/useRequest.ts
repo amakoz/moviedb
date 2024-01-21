e90d3ddb194e3f1ab0a8ffc1d218c86b4569a6f9
amadeuszkozlowski <amadeuszkozlowski@gmail.com>
2024-01-22 00:58:08 +0100

    fix: typing axios composables

--- src/composables/helpers/useRequest.ts
+++ src/composables/helpers/useRequest.ts
@@ -1,167 +1,193 @@
-const METHOD = {
-    GET: 'GET',
-    POST: 'POST',
-    PUT: 'PUT',
-    UPDATE: 'UPDATE',
-    DELETE: 'DELETE',
+import { AxiosInstance, AxiosRequestConfig } from "axios";
+
+export enum METHOD {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  UPDATE = "UPDATE",
+  DELETE = "DELETE",
+}
+
+interface RequestBuilderParams {
+  api: AxiosInstance;
+  method: keyof typeof METHOD;
+  path?: string | null;
+  params?: object;
+  query?: object;
+  body?: object;
+  config?: object;
 }
 
 export default (
-    {
-        api,
-        method,
-        path = null,
-        params: paramsBuilder = {},
-        query: queryBuilder = {},
-        body: bodyBuilder = {},
-        config: configBuilder = {},
-    }: any,
-    {
-        path: forcedPath = null,
-        params: paramsValue = {},
-        query: queryValue = {},
-        body: bodyValue = {},
-        config: configRequest = {}
-    }: any = {},
+  builderParams: RequestBuilderParams,
+  valuesParams: Partial<RequestBuilderParams> = {},
 ): any => {
-    try {
-        return buildRequest(
-            {
-                api,
-                method,
-                path: path,
-                params: paramsBuilder,
-                query: queryBuilder,
-                body: bodyBuilder,
-                config: configBuilder,
-            },
-            {
-                api,
-                path: forcedPath,
-                params: paramsValue,
-                query: queryValue,
-                body: bodyValue,
-                config: configRequest
-            },
-        )
-    } catch (e) {
-        console.error(e)
-        return false
-    }
-}
+  try {
+    return buildRequest(builderParams, valuesParams);
+  } catch (e) {
+    console.error(e);
+    return false;
+  }
+};
 
 function buildRequest(
-    { api, method, path: path, params: paramsBuilder, query: queryBuilder, body: bodyBuilder, config: configBuilder }: any,
-    { path: forcedPath, params: paramsValue, query: queryValue, body: bodyValue, config: configRequest }: any,
+  {
+    api,
+    method,
+    path: path,
+    params: paramsBuilder,
+    query: queryBuilder,
+    body: bodyBuilder,
+    config: configBuilder,
+  }: RequestBuilderParams,
+  {
+    path: forcedPath,
+    params: paramsValue,
+    query: queryValue,
+    body: bodyValue,
+    config: configRequest,
+  }: Partial<RequestBuilderParams>,
 ) {
-    let finalPath = forcedPath || path;
-    method = method.toLowerCase()
+  let finalPath = forcedPath || path;
+  method = method.toLowerCase();
 
-    const config = { ...configBuilder, ...configRequest }
+  const config = { ...configBuilder, ...configRequest };
 
-    if (!paramsBuilder && !queryBuilder && !bodyBuilder) {
-        return requestBuilder(method, path, config)
-    }
+  if (!paramsBuilder && !queryBuilder && !bodyBuilder) {
+    return requestBuilder(method, path, config);
+  }
 
-    //If procedural path configuration
-    if (!forcedPath) {
-        if (paramsBuilder && Object.keys(paramsBuilder).length) {
-            let params = path.split('/').filter((path: any) => path.includes(':'))
-            params = params.map((param: any) => param.replace(':', ''))
-            if (params.length !== Object.keys(paramsBuilder).length) {
-                throw Error(`'params' field is not correctly implementing 'path' params declaration. [Ex1]`)
-            }
-
-            const paramsBuilderKeys = Object.keys(paramsBuilder)
-            for (let key of paramsBuilderKeys) {
-                if (!params.includes(key)) {
-                    throw Error(`'params' field is not correctly implementing 'path' params declaration. [Ex2]`)
-                } else {
-                    if (paramsBuilder[key].required && paramsValue[key] === undefined) {
-                        throw Error(` params '${ key }' is required but not setted in request.`)
-                    } else if (paramsValue[key]) {
-                        if (typeof paramsBuilder[key].format === 'function') {
-                            paramsValue[key] = paramsBuilder[key].format(paramsValue[key])
-                        }
-                        const validated = paramsBuilder[key].validators
-                            ? paramsBuilder[key].validators.some((func: any) => func(paramsValue[key]) === true)
-                            : true
-                        if (validated) {
-                            finalPath = finalPath.replace(`:${ key }`, paramsValue[key])
-                        } else {
-                            throw Error(
-                                ` params '${ key }' is not valid. Required: ${ paramsBuilder[key].validators.join(', ') }`,
-                            )
-                        }
-                    }
-                }
-            }
+  //If procedural path configuration
+  if (!forcedPath) {
+    if (paramsBuilder && Object.keys(paramsBuilder).length) {
+      if (typeof path === "string") {
+        let params = path.split("/").filter((path) => path.includes(":"));
+        params = params.map((param) => param.replace(":", ""));
+        if (params.length !== Object.keys(paramsBuilder).length) {
+          throw Error(
+            `'params' field is not correctly implementing 'path' params declaration. [Ex1]`,
+          );
         }
 
-        if (queryBuilder && Object.keys(queryBuilder).length) {
-            const queryBuilderKeys = Object.keys(queryBuilder)
-            for (let key of queryBuilderKeys) {
-                if (queryBuilder[key].required && queryValue[key] === undefined) {
-                    if (!queryBuilder[key].default)
-                        throw Error(` query '${ key }' is required but not setted in request.`)
-                    else queryValue[key] = queryBuilder[key].default
-                } else if (queryValue[key]) {
-                    if (typeof queryBuilder[key].format === 'function') {
-                        queryValue[key] = queryBuilder[key].format(queryValue[key])
-                    }
-                    const validated = queryBuilder[key].validators
-                        ? queryBuilder[key].validators.some((func: any) => func(queryValue[key]) === true)
-                        : true
-                    if (!validated) {
-                        throw Error(` query '${ key }' is not valid. Required: ${ queryBuilder[key].validators.join(', ') }`)
-                    }
-                }
+        const paramsBuilderKeys = Object.keys(paramsBuilder);
+        for (const key of paramsBuilderKeys) {
+          if (!params.includes(key)) {
+            throw Error(
+              `'params' field is not correctly implementing 'path' params declaration. [Ex2]`,
+            );
+          } else {
+            if (paramsBuilder[key].required && paramsValue[key] === undefined) {
+              throw Error(
+                ` params '${key}' is required but not setted in request.`,
+              );
+            } else if (paramsValue[key]) {
+              if (typeof paramsBuilder[key].format === "function") {
+                paramsValue[key] = paramsBuilder[key].format(paramsValue[key]);
+              }
+              const validated = paramsBuilder[key].validators
+                ? paramsBuilder[key].validators.some(
+                    (func: any) => func(paramsValue[key]) === true,
+                  )
+                : true;
+              if (validated) {
+                finalPath = finalPath.replace(`:${key}`, paramsValue[key]);
+              } else {
+                throw Error(
+                  ` params '${key}' is not valid. Required: ${paramsBuilder[key].validators.join(", ")}`,
+                );
+              }
             }
-            finalPath = finalPath + '?' + new URLSearchParams(queryValue).toString()
+          }
         }
-
-        if (bodyBuilder && Object.keys(bodyBuilder).length) {
-            const bodyBuilderKeys = Object.keys(bodyBuilder)
-            for (let key of bodyBuilderKeys) {
-                if (bodyBuilder[key].required && bodyValue[key] === undefined) {
-                    throw Error(` body '${ key }' is required but not setted in request.`)
-                } else if (bodyValue[key]) {
-                    if (typeof bodyBuilder[key].format === 'function') {
-                        bodyValue[key] = bodyBuilder[key].format(bodyValue[key])
-                    }
-                    const validated = bodyBuilder[key].validators
-                        ? bodyBuilder[key].validators.some((func: any) => func(bodyValue[key]) === true)
-                        : true
-                    if (!validated) {
-                        throw Error(` body '${ key }' is not valid. Required: ${ bodyBuilder[key].validators.join(', ') }`)
-                    }
-                }
-            }
-        }
-    }
-    //If scriptural forced path
-    else {
-        finalPath = forcedPath;
+      }
     }
 
-    return requestBuilder(api, method, finalPath, bodyValue, config)
+    if (queryBuilder && Object.keys(queryBuilder).length) {
+      const queryBuilderKeys = Object.keys(queryBuilder);
+      for (const key of queryBuilderKeys) {
+        if (queryBuilder[key].required && queryValue[key] === undefined) {
+          if (!queryBuilder[key].default)
+            throw Error(
+              ` query '${key}' is required but not setted in request.`,
+            );
+          else queryValue[key] = queryBuilder[key].default;
+        } else if (queryValue[key]) {
+          if (typeof queryBuilder[key].format === "function") {
+            queryValue[key] = queryBuilder[key].format(queryValue[key]);
+          }
+          const validated = queryBuilder[key].validators
+            ? queryBuilder[key].validators.some(
+                (func: any) => func(queryValue[key]) === true,
+              )
+            : true;
+          if (!validated) {
+            throw Error(
+              ` query '${key}' is not valid. Required: ${queryBuilder[key].validators.join(", ")}`,
+            );
+          }
+        }
+      }
+      finalPath = finalPath + "?" + new URLSearchParams(queryValue).toString();
+    }
+
+    if (bodyBuilder && Object.keys(bodyBuilder).length) {
+      const bodyBuilderKeys = Object.keys(bodyBuilder);
+      for (const key of bodyBuilderKeys) {
+        if (bodyBuilder[key].required && bodyValue[key] === undefined) {
+          throw Error(` body '${key}' is required but not setted in request.`);
+        } else if (bodyValue[key]) {
+          if (typeof bodyBuilder[key].format === "function") {
+            bodyValue[key] = bodyBuilder[key].format(bodyValue[key]);
+          }
+          const validated = bodyBuilder[key].validators
+            ? bodyBuilder[key].validators.some(
+                (func: any) => func(bodyValue[key]) === true,
+              )
+            : true;
+          if (!validated) {
+            throw Error(
+              ` body '${key}' is not valid. Required: ${bodyBuilder[key].validators.join(", ")}`,
+            );
+          }
+        }
+      }
+    }
+  }
+  //If scriptural forced path
+  else {
+    finalPath = forcedPath;
+  }
+
+  return requestBuilder(
+    api,
+    method as keyof typeof METHOD,
+    finalPath,
+    bodyValue,
+    config,
+  );
 }
 
-function requestBuilder(api: any, method: any, pathValue: any, bodyValue = null, config: any = {}) {
-    if (config.headers) {
-        config.headers = {
-            ...api.defaults.headers.common,
-            ...api.defaults.headers[method],
-            ...config.headers
-        }
-    }
-    // console.log('useRequest CONFIG',config);
-    return {
-        method: method,
-        path: pathValue,
-        body: JSON.stringify(bodyValue),
-        exec: [ METHOD.GET, METHOD.DELETE ].includes(method)
-            ? () => api[method](pathValue, config)
-            : () => api[method](pathValue, bodyValue, config),
-    }
+function requestBuilder(
+  api: AxiosInstance,
+  method: keyof typeof METHOD,
+  pathValue: string,
+  bodyValue = null,
+  config: AxiosRequestConfig = {},
+) {
+  if (config.headers) {
+    config.headers = {
+      ...api.defaults.headers.common,
+      ...api.defaults.headers[method],
+      ...config.headers,
+    };
+  }
+  // console.log('useRequest CONFIG',config);
+  return {
+    method: method,
+    path: pathValue,
+    body: JSON.stringify(bodyValue),
+    exec: [METHOD.GET, METHOD.DELETE].includes(method)
+      ? () => api[method](pathValue, config)
+      : () => api[method](pathValue, bodyValue, config),
+  };
 }
--- src/composables/useApi.ts
+++ src/composables/useApi.ts
@@ -1,24 +1,54 @@
-import useRequest from './helpers/useRequest';
-import useResponse from './helpers/useResponse';
+import useRequest from "./helpers/useRequest";
+import useResponse from "./helpers/useResponse";
 
-export default (requestConstructor: any, requestValues: any = {}):
-    Promise<{ status: string; isSuccess: boolean; data: any; errors: object; } | any> | any => {
+interface RequestValues {
+  path?: string;
+  params?: object;
+  query?: object;
+  body?: object;
+}
 
-    let response;
-    let valuesKeys:any = requestValues ? Object.keys(requestValues) : {};
+export interface ApiResponse {
+  status: string;
+  isSuccess: boolean;
+  data: any;
+  errors?: object;
+}
 
-    // Shortcut for POST : If no 'params','query','body' keys in requestValues object, so it is 'body' object itself.
-    if (valuesKeys.length && !valuesKeys.some((key: any) => ['path', 'params', 'query', 'body'].includes(key))) {
-        const body = { ...requestValues };
-        requestValues = {};
-        requestValues.body = body;
-    }
+export default (
+  requestConstructor: any,
+  requestValues: RequestValues = {},
+): Promise<ApiResponse> => {
+  let response;
+  const valuesKeys: any = requestValues ? Object.keys(requestValues) : {};
 
-    const request = useRequest(requestConstructor, requestValues);
-    console.log('►►►', request.method.toUpperCase(), request.path, requestConstructor, requestValues);
+  // Shortcut for POST : If no 'params','query','body' keys in requestValues object, so it is 'body' object itself.
+  if (
+    valuesKeys.length &&
+    !valuesKeys.some((key: any) =>
+      ["path", "params", "query", "body"].includes(key),
+    )
+  ) {
+    const body = { ...requestValues };
+    requestValues = {};
+    requestValues.body = body;
+  }
 
-    if (request)
-        response = useResponse({ method: request.method, path: request.path, exec: request.exec });
-    else throw new Error('Something went wront with your API request.');
-    return response;
+  const request = useRequest(requestConstructor, requestValues);
+  console.log(
+    "►►►",
+    request.method.toUpperCase(),
+    request.path,
+    requestConstructor,
+    requestValues,
+  );
+
+  if (request)
+    response = useResponse({
+      method: request.method,
+      path: request.path,
+      exec: request.exec,
+    });
+  else throw new Error("Something went wront with your API request.");
+  return response;
 };
